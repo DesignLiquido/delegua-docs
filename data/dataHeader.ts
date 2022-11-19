1546fc2f9c0a56463a82a5ad1c2c36fb02c91b6d
ItaloCobains <monokatchaleger@gmail.com>
2022-11-19 12:56:02 -0300

    Limpando codigo

---
+++ data/dataHeader.ts
@@ -0,0 +1,37 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { ImNpm } from "react-icons/im";
+
+export const data = {
+  social: [
+    {
+      title: "Docs",
+      link: "",
+      id: 1,
+    },
+    {
+      title: "Bibliotecas",
+      link: "",
+      id: 2,
+    },
+    {
+      title: "Experimente Online",
+      link: "",
+      id: 3,
+    },
+    {
+      title: BsGithub,
+      link: "",
+      id: 4,
+    },
+    {
+      title: BsLinkedin,
+      link: "",
+      id: 5,
+    },
+    {
+      title: ImNpm,
+      link: "",
+      id: 6,
+    },
+  ],
+};
