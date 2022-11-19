9aaf0e4e822fc405d27ed341aef79aa451eda918
Kennedy <zelpzxhacker@gmail.com>
2022-11-19 14:12:10 -0300

    Nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,12 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import type { AppProps } from "next/app";
 
+// NProgress
 Router.events.on("routeChangeStart", (url) => {
   console.log(`Loading ${url}`);
   NProgress.start();
 });
-
 Router.events.on("routeChangeComplete", () => NProgress.done());
-
 Router.events.on("routeChangeError", () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
