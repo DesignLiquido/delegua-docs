702c4a532332465ebb0096df99bccd12ccada75c
Kennedy <zelpzxhacker@gmail.com>
2022-11-19 14:04:30 -0300

    Textos da home fixados

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Sidebar from "./Sidebar";
-import React from 'react'
+import React from "react";
 
 // Asset's
 import logo from "../assets/images/dl_logo.png";
@@ -21,36 +21,43 @@ const Header = () => {
     <>
       <header
         className="w-screen flex justify-around items-center h-20
-        shadow-2xl backdrop-blur-md">
+        shadow-2xl backdrop-blur-md"
+      >
         <div>
           <Image src={logo} width={250} alt="" priority={true} />
         </div>
 
         <nav
           className="flex items-center justify-center gap-2 text-white
-          text-xl">
-          
+          text-xl"
+        >
           <div className="flex max-[900px]:hidden">
             {data.social.map((data) => {
-              return(
-                <Link key={data.id} className="p-2 rounded-lg transition-all hover:bg-[#4e99c5]" href={data.link}>
-                  {typeof data.title === 'string' ? data.title : React.createElement(data.title, {
-                    size: 25
-                  })}
+              return (
+                <Link
+                  key={data.id}
+                  className="p-2 rounded-lg transition-all hover:bg-[#4e99c5]"
+                  href={data.link}
+                >
+                  {typeof data.title === "string"
+                    ? data.title
+                    : React.createElement(data.title, {
+                        size: 25,
+                      })}
                 </Link>
-              )
+              );
             })}
-            </div>
+          </div>
           <Link
             className="p-2 rounded-lg transition-all hover:bg-[#4e99c5] min-[900px]:hidden"
             href={""}
             onClick={handleToggleSidebar}
           >
-            <FiMenu size={25}  />
+            <FiMenu size={25} />
           </Link>
         </nav>
       </header>
-      
+
       <Sidebar isOpen={sidebarIsOpen} />
     </>
   );
